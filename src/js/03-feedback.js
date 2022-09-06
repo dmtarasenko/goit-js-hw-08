94286edd9b954df3321b09ced9a575ffa0199010
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-09-06 23:34:08 +0300

    task3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onInputForm, 500));
+formEl.addEventListener('submit', onSubmitForm);
+
+if (localStorage.getItem('feedback-form-state')) {
+  formEl.elements.email.value = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).email;
+
+  formEl.elements.message.value = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
+}
+
+function onInputForm() {
+  const feedbackForm = {
+    email: formEl.elements.email.value,
+    message: formEl.elements.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackForm));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+}

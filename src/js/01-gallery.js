20a412a4b1a13d98af0a185ddabcb38d000e4a3e
Dmytro Tarasenko <96738953+dmtarasenko@users.noreply.github.com>
2022-09-06 20:15:24 +0300

    task1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,5 +7,31 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const galleryEl = document.querySelector('div.gallery');
+
+galleryItemsCreator(galleryItems);
+
+function galleryItemsCreator(items) {
+  const gallaryMarkup = items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"   
+                />
+            </a>`
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', gallaryMarkup);
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+  caption: true,
+  nav: true,
+});

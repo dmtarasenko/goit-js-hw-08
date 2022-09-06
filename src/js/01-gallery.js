0a985d627208c2e6803ee2050812b193033d1cf1
Dmytro Tarasenko <96738953+dmtarasenko@users.noreply.github.com>
2022-09-06 20:04:12 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,10 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+console.log(SimpleLightbox);

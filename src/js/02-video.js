b7411b0b3602fb17fdc2dcdf265c7337d1886c4a
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-09-07 22:07:18 +0300

    corrects error in console

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,10 @@ const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
 
 player.on('timeupdate', throttle(onPlayerTimeUpdate, 1000));
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
 
 function onPlayerTimeUpdate(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
